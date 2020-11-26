12f4060a6b92e3b96996572b5afd2e12a0acdaa9
gabb134 <cg.marrero@cgodin.qc.ca>
2020-11-26 11:54:15 -0500

    modif gab projet expo en cours

--- App.js
+++ App.js
@@ -2,23 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Image,StyleSheet, Text, View } from 'react-native';
 import logo from './assets/favicon.png'; 
+import ListeUtilisateurs from './Components/ListeUtilisateurs'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Bonjour tour le monde!!!</Text>
-      <Text>Allo!!</Text>
-      <Image source={logo} style={{ width: 305, height: 159 }} /> 
-      <StatusBar style="auto" />
-    </View>
+    <ListeUtilisateurs/>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
